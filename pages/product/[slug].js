05f1e833aa96e109e1d7050d756af5b0202d1cfa
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-29 13:35:48 +0100

    added caroosee effect to product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,27 +1,30 @@
 //using [] to name it means its gonna be dynamic
 
-import React from 'react';
+import React, {useState} from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor} from '../../lib/client';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products}) => {
     const { image, name, details, price } = product;
+    const [index, setIndext] = useState(0);
     return (
         <div>
             <div className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={ urlFor(image && image[0])} />
+                        <img src={ urlFor(image && image[index])} />
                     </div>
-                    {/* <div className='small-images-container'>
+                    <div className='small-images-container'>
                         {image?.map((item, i) => (
                             <img src={urlFor(item)}
-                            className=""
-                            onMouseEnter=""
+                            className={i === index ? 'small-image selected-image' : 
+                            'small-image'}
+                            onMouseEnter={()=> setIndext(i)}
                             />
                         ))}
-                    </div> */}
+                    </div>
                 </div>
                 <div className='product-detail-desc'>
                     <h1>{name}</h1>
@@ -62,6 +65,17 @@ const ProductDetails = ({ product, products}) => {
                         className='buy-now'
                         onClick=''>Buy now
                         </button>
+                    </div>
+                </div>
+            </div>
+            <div className='maylike-products-wrapper'>
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div className='maylike-products-container track'>
+                        {products.map((item) => (
+                            <Product key={item._id}
+                            product={item} />
+                        ))}
                     </div>
                 </div>
             </div>
