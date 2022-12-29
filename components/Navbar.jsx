0d4d247847f1a6827ae834dea0e6c79a43d76f1e
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-29 15:45:45 +0100

    added use context and impleted product update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">KSM Headphones</Link>
       </p>
       <button type='button'
       className='cart-icon' onclick=''>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,16 +5,20 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor} from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products}) => {
     const { image, name, details, price } = product;
     const [index, setIndext] = useState(0);
+    const {decQty, incQty, qty} = useStateContext();
+
+
     return (
         <div>
             <div className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={ urlFor(image && image[index])} />
+                        <img src={ urlFor(image && image[index])} className='product-detail-image'/>
                     </div>
                     <div className='small-images-container'>
                         {image?.map((item, i) => (
@@ -26,48 +30,40 @@ const ProductDetails = ({ product, products}) => {
                         ))}
                     </div>
                 </div>
-                <div className='product-detail-desc'>
-                    <h1>{name}</h1>
-                    <div className='reviews'>
-                        <div>
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiOutlineStar />
-                        </div>
-                        <p>
-                            (20)
-                        </p>
-                    </div>
-                    <h4>Details</h4>
-                    <p>{details}</p>
-                    <p className='price'>₦{price}</p>
-                    <div className='quantity'>
-                        <h3>Quantity:</h3>
-                        <p className='quantity-desc'>
-                            <span className='minus' onClick="">
-                                <AiOutlineMinus />
-                            </span>
-                            <span className='num' onClick="">
-                                0
-                            </span>
-                            <span className='plus' onClick="">
-                                <AiOutlinePlus />
-                            </span>
-                        </p>
-                    </div>
-                    <div className='buttons'>
-                        <button type='button' 
-                        className='add-to-cart' 
-                        onClick=''>Add to Cart</button>
-                        <button type='button' 
-                        className='buy-now'
-                        onClick=''>Buy now
-                        </button>
-                    </div>
-                </div>
+               
+                <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
             </div>
+            <p>
+              (20)
+            </p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="buy-now" onClick=''>Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+
             <div className='maylike-products-wrapper'>
                 <h2>You may also like</h2>
                 <div className='marquee'>
@@ -119,3 +115,5 @@ export const getStaticProps = async ({params: { slug }}) => {
   }
 
 export default ProductDetails
+
+
