dc190ebcee08c76900ce2f339b13894de9a489a9
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-29 16:22:19 +0100

    stopped for today 2.02

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Link from 'next/Link';
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShopping } from 'react-icons/ai';
+
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href="/">KSM Headphones</Link>
       </p>
       <button type='button'
-      className='cart-icon' onclick=''>
+      className='cart-icon' onclick={()=> setShowCart(true)}>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
