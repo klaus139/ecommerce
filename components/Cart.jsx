b764f6978ff01578fe9cd7e8c8610fa986177044
Nicholas igunbor <nickoklaus5@gmail.com>
2022-12-30 11:27:46 +0100

    finished the state context component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,7 +11,7 @@ import { urlFor } from '../lib/client';
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity } = useStateContext();
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -53,18 +53,41 @@ const Cart = () => {
                 <div className='flex bottom'>
                   <div>
                     <p className="quantity-desc">
-                      <span className="minus" onClick=''><AiOutlineMinus /></span>
-                      <span className="num">{qty}</span>
-                      <span className="plus" onClick=''><AiOutlinePlus /></span>
+                      <span className="minus" 
+                      onClick={() => toggleCartItemQuantity(item._id, 'dec')}>
+                        <AiOutlineMinus />
+                      </span>
+                      <span className="num">{item.totalQuantity}</span>
+                      <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
+                        <AiOutlinePlus />
+                      </span>
                     </p>
                   </div>
+                  <button type='button'
+                  className='remove-item'
+                  onClick=''>
+                    <TiDeleteOutline />
+                  </button>
                 </div>
               </div>
             </div>
           ))}
         </div>
+        {cartItems.length >= 1 && (
+          <div className='cart-bottom'>
+            <div className='total'>
+              <h3>Subtoal:</h3>
+              <h3>₦{totalPrice}</h3>
+            </div>
+            <div className='btn-container'>
+              <button type='button'
+              className='btn' onClick=''>
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-
     </div>
   )
 }
